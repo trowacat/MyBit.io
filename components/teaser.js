fa4636448c58dcc052bad235fc558aec40de9d31
Jose Aguinaga <jj.perez.aguinaga@gmail.com>
2018-05-10 04:31:33 -0500

    Added a more beautiful presentation for the teaser and instructions for everyon to know

--- components/teaser.js
+++ components/teaser.js
@@ -1,17 +1,31 @@
 import React from 'react'
+import MyBitLogo from '../static/svgs/icons/mybit-full-white.svg'
 import stylesheet from './teaser.scss'
+import { Button } from './button';
 
 export const Teaser = () => (
-  <div>
+  <div className="Teaser">
     <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
     <div className='Teaser__wrapper' >
       <div className='Teaser__container'>
-        <video
-          loop
-          autoPlay
-          poster='https://files.mybit.io/ownyourfuture.png'
-          src='https://files.mybit.io/ownyourfuture.mp4'
-        />
+        <div className='Teaser__logo'>
+          <MyBitLogo />
+        </div>
+        <h1 className='Teaser__title'>
+          New Site migration in process.<br/>
+          Please check back later!
+        </h1>
+        <p className='Teaser__paragraph'>
+          The MYB token swap is now live, the Token Swap will be exchanging the ERC20 MYB tokens with the new updated coin also with the symbol MYB. For more informaton on the token swap please read our blog post by clicking ‘learn more’.
+        </p>
+        <div className='Teaser__buttons'>
+          <div className='Teaser__button'>
+            <Button isLight label='Learn more' />
+          </div>
+          <a rel="noopener noreferrer" target="_blank" className='Teaser__button' href='https://medium.com/mybit-dapp/as-we-are-on-the-brink-of-entering-mybits-new-era-we-have-a-very-important-announcement-to-make-a4cda1cd6956'>
+            <Button isLight label='See why' />
+          </a>
+        </div>
       </div>
     </div>
   </div>
